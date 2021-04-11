e4ce8db7165c5d85d16687220268f5409382dd5e
Juan Manuel Restrepo Moreno <juanpix2404@gmail.com>
2021-04-11 00:11:44 -0500

    Creacion componente item

---
+++ src/components/Item.vue
@@ -0,0 +1,26 @@
+<template>
+   <tr>
+    <th scope="row">Id</th>
+    <td>title</td>
+    <td>Amount</td>
+    <td>
+        <button class="btn btn-info btn-sm">
+            +
+        </button>
+        <button class="btn btn-danger btn-sm">
+            -
+        </button>
+    </td>
+    <td>$ test</td>
+  </tr>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
